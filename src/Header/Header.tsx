2c56fd6bbd1d6dcaf901ce05cda8cb02f66421c7
Arseny Belyaev <ars-bel@mail.ru>
2023-04-21 00:11:37 +0300

    css modules

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styles from './Header.module.css';
 
 
 export default function Header() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Магазин</Link>
-                </li>
-                <li>
-                    <Link to="/about">О себе</Link>
-                </li>
-                <li>
-                    <Link to="/studio">Мастерская</Link>
-                </li>
-                <li>
-                    <Link to="/events">Мероприятия</Link>
-                </li>
-                <li>
-                    <Link to="/contacts">Контакты</Link>
-                </li>
-            </ul>
-        </nav>
+        <header>
+            <div className={styles.wrap}>
+                <h1>Катя С.</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Магазин</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">О себе</Link>
+                        </li>
+                        <li>
+                            <Link to="/studio">Мастерская</Link>
+                        </li>
+                        <li>
+                            <Link to="/events">Мероприятия</Link>
+                        </li>
+                        <li>
+                            <Link to="/contacts">Контакты</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 }
